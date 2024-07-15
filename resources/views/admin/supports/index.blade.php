a2dd4ccf2cd3d4bf2136749937436f32efa27fc0
Karoline Gaia Alexandre <drakarolinegaia@gmail.com>
2024-07-15 07:25:35 -0300

    Criando página suporte

--- resources/views/admin/supports/index.blade.php
+++ resources/views/admin/supports/index.blade.php
@@ -1,1 +1,19 @@
-<h1>AQUI</h1>
+<h1>Listagem de Suporte</h1>
+<a href="{{route ('supports.create') }}">Criar Dúvida</a>
+<table>
+    <thead>
+        <th>assunto</th>
+        <th>status</th>
+        <th>descrição</th>
+    </thead>
+</table>
+<tbody>
+@foreach($supports as $support)
+    <tr>
+        <td>{{$support->subject}}</td>
+        <td>{{$support->status}}</td>
+        <td>{{$support->body}}</td>
+        <td> > </td>
+    </tr>
+@endforeach
+</tbody>
